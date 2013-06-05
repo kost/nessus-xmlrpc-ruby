c703c691ceef5eac9262d8ea1e01c65d94a40a73
George Ehrhorn <ehrhorn@gmail.com>
2013-06-05 11:41:51 -0400

    Fixed xpath in policy_get_id. policyID was being treated as a parent node of policyName We need to supply the policyName as a predicate and select the value of policyName.text

--- lib/nessus-xmlrpc.rb
+++ lib/nessus-xmlrpc.rb
@@ -505,7 +505,7 @@ class NessusXMLRPCnokogiri < NessusXMLRPCrexml
 	def policy_get_id(textname) 
 		post= { "token" => @token } 
 		docxml=nessus_request('policy/list', post)
-		return docxml.xpath("/reply/contents/policies/policy/policyName[text()='"+textname+"']/..policyID").collect(&:text)[0]
+    return docxml.xpath("/reply/contents/policies/policy[policyName='"+textname+"']/policyID").collect(&:text)
 	end	
 
 	def policy_list_uids
